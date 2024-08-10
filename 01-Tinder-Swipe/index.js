66a6aa4c97873f303342831d4de53fccd42aa1e6
javierOrtega95 <fjortega@wizzie.io>
2024-08-10 20:52:17 +0200

    feat(01-Tinder-Swipe): calculate drag distance and apply transformation for swipe effect

--- 01-Tinder-Swipe/index.js
+++ 01-Tinder-Swipe/index.js
@@ -1,39 +1,57 @@
 // listen to the mouse drag and touch event
-document.addEventListener('mousedown', onDrag);
-document.addEventListener('touchstart', onDrag, { passive: true });
+document.addEventListener('mousedown', onDrag)
+document.addEventListener('touchstart', onDrag, { passive: true })
+
+let isDragging = false
+const ROTATION_FACTOR = 15
 
 function onDrag(event) {
-  const currentCard = event.target.closest('article');
+  if (isDragging) return
 
-  if (!currentCard) return;
+  const currentCard = event.target.closest('.card')
 
-  addDragEventListeners();
+  if (!currentCard) return
+
+  // get initial position of mouse or finger
+  const startX = event.pageX ?? event.touches[0].pageX
+
+  addDragEventListeners()
 
   function onMove(event) {
-    console.log('Moving...');
+    const currentX = event.pageX ?? event.touches[0].pageX
+    const dragOffsetX = currentX - startX
+
+    // exit if there's no horizontal movement
+    if (dragOffsetX === 0) return
+
+    isDragging = true
+
+    const rotationDegrees = dragOffsetX / ROTATION_FACTOR
+
+    // apply the transformation to the card
+    currentCard.style.transform = `translateX(${dragOffsetX}px) rotate(${rotationDegrees}deg)`
+    currentCard.style.cursor = 'grabbing'
   }
 
   function onMoveEnd(event) {
-    console.log('Move end');
-
-    removeDragEventListeners();
+    removeDragEventListeners()
   }
 
   function addDragEventListeners() {
     // listen the mouse and touch movements
-    document.addEventListener('mousemove', onMove);
-    document.addEventListener('mouseup', onMoveEnd);
+    document.addEventListener('mousemove', onMove)
+    document.addEventListener('mouseup', onMoveEnd)
 
-    document.addEventListener('touchmove', onMove, { passive: true });
-    document.addEventListener('touchend', onMoveEnd, { passive: true });
+    document.addEventListener('touchmove', onMove, { passive: true })
+    document.addEventListener('touchend', onMoveEnd, { passive: true })
   }
 
   function removeDragEventListeners() {
     // remove the mouse and touch event listeners
-    document.removeEventListener('mousemove', onMove);
-    document.removeEventListener('mouseup', onMoveEnd);
+    document.removeEventListener('mousemove', onMove)
+    document.removeEventListener('mouseup', onMoveEnd)
 
-    document.removeEventListener('touchmove', onMove);
-    document.removeEventListener('touchend', onMoveEnd);
+    document.removeEventListener('touchmove', onMove)
+    document.removeEventListener('touchend', onMoveEnd)
   }
 }
