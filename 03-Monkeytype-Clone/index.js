144a457b70c362a28ff848b963b17d5fe3a8d8c2
javierOrtega95 <fjortega@wizzie.io>
2024-10-13 14:29:02 +0200

    feat(03-Monkeytype-Clone): add active word and cursor positioning functionality

--- 03-Monkeytype-Clone/index.js
+++ 03-Monkeytype-Clone/index.js
@@ -1,11 +1,14 @@
 import { words as INITIAL_WORDS } from './data.js'
+
 const $time = document.querySelector('time')
 const $paragraph = document.querySelector('.words')
+const $input = document.querySelector('input')
 
 const INITIAL_TIME = 30
 let currentTime = INITIAL_TIME
 
 initGame()
+initEvents()
 
 function initGame() {
   const words = INITIAL_WORDS.toSorted(() => Math.random() - 0.5).slice(0, 50)
@@ -24,8 +27,15 @@ function initGame() {
 
   const firstWord = document.querySelector('monkey-word')
   firstWord.classList.add('active')
+
   const firstLetter = document.querySelector('monkey-letter')
   firstLetter.classList.add('active')
+}
+
+function initEvents() {
+  document.addEventListener('keydown', () => {
+    $input.focus()
+  })
 
   const intervalId = setInterval(() => {
     currentTime--
@@ -36,6 +46,41 @@ function initGame() {
       gameOver('Game Over')
     }
   }, 1000)
+
+  $input.addEventListener('keydown', onKeyDown)
+  $input.addEventListener('keyup', onKeyUp)
+}
+
+function onKeyDown() {} // TODO: handle Backspace
+function onKeyUp() {
+  const $currentWord = $paragraph.querySelector('monkey-word.active')
+  const $currentLetter = $currentWord.querySelector('monkey-letter.active')
+  const $allLetters = $currentWord.querySelectorAll('monkey-letter')
+  const currentWordValue = $currentWord.innerText.trim()
+  const { value: inputValue } = $input
+  const inputLetters = inputValue.split('')
+
+  $input.maxLength = currentWordValue.length
+  $allLetters.forEach(($letter) => $letter.classList.remove('correct', 'incorrect'))
+
+  inputLetters.forEach((char, index) => {
+    const $letter = $allLetters[index]
+    const letterToCheck = currentWordValue[index]
+
+    const isCorrect = char === letterToCheck
+    const letterClass = isCorrect ? 'correct' : 'incorrect'
+    $letter.classList.add(letterClass)
+  })
+
+  $currentLetter.classList.remove('active', 'is-last')
+  const $nextActiveLetter = $allLetters[inputValue.length]
+
+  if ($nextActiveLetter) {
+    $nextActiveLetter.classList.add('active')
+  } else {
+    $currentLetter.classList.add('active', 'is-last')
+    // TODO: gameOver if there is no next word
+  }
 }
 
 function gameOver() {
